79f3c65384919fb01cd1a3d56818e62185c7d4b0
eidanyosoy <77819847+eidanyosoy@users.noreply.github.com>
2021-04-12 18:09:48 -0400

    Add files via upload

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+
+    "github.com/docker/go-plugins-helpers/volume"
+)
+
+func main() {
+    driver := newLocalPersistDriver()
+
+    handler := volume.NewHandler(driver)
+    fmt.Println(handler.ServeUnix("root", driver.name))
+}
